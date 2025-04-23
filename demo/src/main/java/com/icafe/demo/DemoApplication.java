009774af43d305f10b01a13848c336c522530258
dzungvd98 <yungvd98@gmail.com>
2025-04-23 21:55:16 +0700

    Update js

--- demo/src/main/java/com/icafe/demo/DemoApplication.java
+++ demo/src/main/java/com/icafe/demo/DemoApplication.java
@@ -2,13 +2,22 @@ package com.icafe.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableWebMvc
 @ComponentScan(basePackages = "com.icafe.demo")
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer{
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
